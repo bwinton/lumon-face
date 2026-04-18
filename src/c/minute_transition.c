5429b46061dd65042a5429305d0dcb98024a5fb8
Blake Winton <bwinton@latte.ca>
2026-04-17 23:42:05 -0400

    Some small box tweaks.

--- src/c/minute_transition.c
+++ src/c/minute_transition.c
@@ -73,10 +73,13 @@ static void prv_end_special_animation(TextLayer *grid_layers[])
 
 static void prv_start_special_animation(const struct tm *tick_time, TextLayer *grid_layers[], char grid_buffer[][2])
 {
-  int label_base = (rand() % 5) + 1;
+  int day = tick_time->tm_mday;
+  int month = tick_time->tm_mon + 1;
+  int values[2] = {day, month};
+  
   for (int i = 0; i < 2; i++)
   {
-    snprintf(s_special_box_text[i], sizeof(s_special_box_text[i]), "%02d", label_base + i);
+    snprintf(s_special_box_text[i], sizeof(s_special_box_text[i]), "%02d", values[i]);
     text_layer_set_text(s_special_box_layers[i], s_special_box_text[i]);
     layer_set_hidden(s_special_box_bg_layers[i], false);
     layer_set_hidden(text_layer_get_layer(s_special_box_layers[i]), false);
@@ -118,21 +121,27 @@ static void prv_start_special_animation(const struct tm *tick_time, TextLayer *g
 
 void minute_transition_init(GRect bounds, int cell_height)
 {
+  const int side_padding = 4;
+  const int bottom_padding = 4;
+  const int box_width = (bounds.size.w / 2) - (side_padding * 2);
+  const int box_height = cell_height - bottom_padding;
+
   for (int i = 0; i < 2; i++)
   {
-    s_special_box_bg_layers[i] = layer_create(GRect(i * (bounds.size.w / 2), bounds.size.h, bounds.size.w / 2, cell_height));
+    int box_x = (i * (bounds.size.w / 2)) + side_padding;
+    s_special_box_bg_layers[i] = layer_create(GRect(box_x, bounds.size.h, box_width, box_height));
     layer_set_update_proc(s_special_box_bg_layers[i], prv_special_box_draw);
     layer_set_hidden(s_special_box_bg_layers[i], true);
 
-    s_special_box_layers[i] = text_layer_create(GRect(i * (bounds.size.w / 2), bounds.size.h, bounds.size.w / 2, cell_height));
+    s_special_box_layers[i] = text_layer_create(GRect(box_x, bounds.size.h, box_width, box_height));
     text_layer_set_background_color(s_special_box_layers[i], GColorClear);
     text_layer_set_text_color(s_special_box_layers[i], LIGHT_BLUE);
     text_layer_set_text_alignment(s_special_box_layers[i], GTextAlignmentCenter);
     text_layer_set_font(s_special_box_layers[i], fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
     layer_set_hidden(text_layer_get_layer(s_special_box_layers[i]), true);
 
-    s_special_box_start[i] = GRect(i * (bounds.size.w / 2), bounds.size.h, bounds.size.w / 2, cell_height);
-    s_special_box_dest[i] = GRect(i * (bounds.size.w / 2), bounds.size.h - cell_height - 8, bounds.size.w / 2, cell_height);
+    s_special_box_start[i] = GRect(box_x, bounds.size.h, box_width, box_height);
+    s_special_box_dest[i] = GRect(box_x, bounds.size.h - box_height - bottom_padding, box_width, box_height);
   }
 }
 
@@ -204,15 +213,36 @@ void minute_transition_update_animation(TextLayer *grid_layers[], const GRect gr
   {
     s_special_frame++;
     int total = s_special_frames_time_move;
+
     for (int j = 0; j < 4; j++)
     {
       int index = s_time_positions[j];
-      int16_t x = s_time_move_orig[j].origin.x + ((s_time_move_dest[j].origin.x - s_time_move_orig[j].origin.x) * s_special_frame) / total;
-      int16_t y = s_time_move_orig[j].origin.y + ((s_time_move_dest[j].origin.y - s_time_move_orig[j].origin.y) * s_special_frame) / total;
-      int16_t w = s_time_move_orig[j].size.w + ((s_time_move_dest[j].size.w - s_time_move_orig[j].size.w) * s_special_frame) / total;
-      int16_t h = s_time_move_orig[j].size.h + ((s_time_move_dest[j].size.h - s_time_move_orig[j].size.h) * s_special_frame) / total;
-      GRect frame = GRect(x, y, w, h);
-      layer_set_frame(text_layer_get_layer(grid_layers[index]), frame);
+      int start_delay = 0; // all digits move together
+      int frame = s_special_frame - start_delay;
+      if (frame < 0)
+      {
+        layer_set_frame(text_layer_get_layer(grid_layers[index]), s_time_move_orig[j]);
+        continue;
+      }
+
+      int travel_total = total - start_delay;
+      if (travel_total < 1)
+        travel_total = 1;
+      if (frame > travel_total)
+        frame = travel_total;
+
+      int x_progress_num = frame * 14;
+      int x_progress_den = travel_total * 10;
+      if (x_progress_num > x_progress_den)
+        x_progress_num = x_progress_den;
+
+      int16_t x = s_time_move_orig[j].origin.x + ((s_time_move_dest[j].origin.x - s_time_move_orig[j].origin.x) * x_progress_num) / x_progress_den;
+      int16_t y = s_time_move_orig[j].origin.y + ((s_time_move_dest[j].origin.y - s_time_move_orig[j].origin.y) * frame) / travel_total;
+      int16_t w = s_time_move_orig[j].size.w + ((s_time_move_dest[j].size.w - s_time_move_orig[j].size.w) * x_progress_num) / x_progress_den;
+      int16_t h = s_time_move_orig[j].size.h + ((s_time_move_dest[j].size.h - s_time_move_orig[j].size.h) * frame) / travel_total;
+
+      GRect frame_rect = GRect(x, y, w, h);
+      layer_set_frame(text_layer_get_layer(grid_layers[index]), frame_rect);
     }
     if (s_special_frame >= total)
     {
